ce433ca0451e3549a9668b48fd6677045c1ecfa3
Justice Shultz <shultzjc1@gmail.com>
2019-09-18 08:45:55 -0700

    Removed problem file

--- GameEngineGeometry/Assets/Scripts/ToDo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "To Do", menuName = "To Do List", order = 1)]
-public class ToDo : ScriptableObject
-{
-    public List<string> toDoList = new List<string>() { "To Do Item #1 Goes Here" };
-}
